e98f0b4333084461ce529791f9ba6b02f7dd09d0
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-05-08 16:13:03 -0300

    feat: add input data validation logic to the login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,60 @@
 "use client";
 import styles from './page.module.css';
 import { Mail, Lock } from 'lucide-react';
+import { useState } from 'react';
 
-export default function Login() {
+interface LoginProps {
+    onSubmit: (email: string, password: string) => void;
+}
+
+export default function Login(props: LoginProps) {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        props.onSubmit(email, password);
+    };
+
+    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
     return ( 
         <div className={styles.pageContainer}>
             <div className={styles.loginContainer}>
                 <h1 className={styles.loginText}>Faça seu login na plataforma</h1>
-                <form className={styles.formContainer}>
+                <form className={styles.formContainer} onSubmit={handleSubmit}>
                     <div className={styles.fieldContainer}>
-                        <Mail className={styles.iconContainer} size={24}/>
-                        <input className={styles.inputContainer} type="email" id="email" name="email" placeholder="email@codexjr.com.br"/>
+                        <Mail className={styles.iconContainer} size={24} />
+                        <input 
+                            className={styles.inputContainer}
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="email@codexjr.com.br"
+                            value={email}
+                            onChange={handleEmailChange}
+                            required
+                        />
                     </div>
                     <div className={styles.fieldContainer}>
-                        <Lock className={styles.iconContainer} size={24}/>
-                        <input className={styles.inputContainer} type="password" id="password" name="password" placeholder="password"/>
+                        <Lock className={styles.iconContainer} size={24} />
+                        <input 
+                            className={styles.inputContainer}
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
                     </div>
-                    <input className={styles.loginButton} type="submit" value="Login"/>
+                    <input className={styles.loginButton} type="submit" value="Login" />
                 </form>
             </div>
         </div>
