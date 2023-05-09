b5c4f17e59212c9c5adb8ebea5b1555a629da343
Igor Souza <igorrs.rib@gmail.com>
2023-05-09 16:46:46 -0300

    style: add container relatorio

--- src/components/relatorios/relatorios.tsx
+++ src/components/relatorios/relatorios.tsx
@@ -2,10 +2,13 @@ import styles from "./page.module.css"
 
 export default function Relatorios(){
     return(
-           <div className={styles.container}>
-            <div className={styles.headerRelatorio}>
-                
-            </div>
+            <div className={styles.container}>
+                <div className={styles.headerRelatorio}>
+
+                </div>
+                <div className={styles.containerRelatorio}>
+
+                </div>
            </div>
     )
 }
