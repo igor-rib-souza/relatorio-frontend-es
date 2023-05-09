7598c9b4d1c6e4e61cc38d250b6982ade97414fc
Igor Souza <igorrs.rib@gmail.com>
2023-05-09 16:13:09 -0300

    feat: create component relatorio

---
+++ src/components/relatorios/relatorios.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css"
+
+export default function Relatorios(){
+    return(
+           <div className={styles.container}>
+            <div className={styles.headerRelatorio}>
+                
+            </div>
+           </div>
+    )
+}
