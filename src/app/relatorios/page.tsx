1866070ab5baac028b701693efe5e3c40103c8c7
Igor Souza <igorrs.rib@gmail.com>
2023-05-09 16:13:48 -0300

    feat: add relatorio component to relatorios page

--- src/app/relatorios/page.tsx
+++ src/app/relatorios/page.tsx
@@ -2,6 +2,7 @@ import Header from "../../components/header/header";
 import Menu from "../../components/menu/menu";
 import Footer from "../../components/footer/footer";
 import styles from "./page.module.css";
+import Relatorio from "../../components/relatorios/relatorios";
 
 export default function Relatorios(){
     return(
@@ -11,7 +12,10 @@ export default function Relatorios(){
                 <div>
                     <Menu/>
                 </div>
-                <Footer/>
+                <div className={styles.container3}>
+                    <Relatorio/>
+                    <Footer/>
+                </div>
             </div>
         </div>
     )
