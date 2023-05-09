6c17269d8a92bb9304dc1281534c4abdc467738b
Igor Souza <igorrs.rib@gmail.com>
2023-05-09 18:03:01 -0300

    style: change button menu on mobile

--- src/components/buttonMenu/buttonMenu.tsx
+++ src/components/buttonMenu/buttonMenu.tsx
@@ -1,11 +1,24 @@
+"use client";
 import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal, PromiseLikeOfReactNode } from "react";
 import styles from "./page.module.css";
 import { Users, Newspaper, Tag, LogOut } from "lucide-react";
+import { useState, useEffect } from 'react';
+
 
 export default function ButtonMenu(props: { text: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | PromiseLikeOfReactNode | null | undefined; }){
+    
+    const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+
+    useEffect(()=>{
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    },[])
+
     return(
             <div>
-                <button className={styles.button}>
+                <button className={styles.button} onClick={()=> console.log(windowSize)}>
                     {
                         props.text == "Relatórios" ? <Newspaper className={styles.ButtonMenu}/>
                         :
@@ -17,7 +30,12 @@ export default function ButtonMenu(props: { text: string | number | boolean | Re
                         :
                         <></>
                     }
-                    <p className={styles.text}>{props.text}</p>
+                    {
+                        windowSize.width > 309 ?
+                        <p className={styles.text}>{props.text}</p>
+                        :
+                        <></>
+                    }
                 </button>
            </div>
     )
