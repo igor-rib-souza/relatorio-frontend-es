5b272e19f0a90eab128c6d236ca32cd139c42917
Igor Souza <igorrs.rib@gmail.com>
2023-05-09 18:28:51 -0300

    style: change mobile design

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,28 @@
+"use client";
 import styles from "./page.module.css"
 import Logo from "../../../public/assets/Logo.png"
+import LogoMinimalista from "../../../public/assets/logo-minimalista.png"
 import Ausente from "../../../public/assets/foto-usuario-ausente.jpg"
 import Image from 'next/image';
 import { Search } from 'lucide-react';
+import { useState, useEffect } from 'react';
 
 
 export default function Header(){
+
+    const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+
+    useEffect(()=>{
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    },[])
+
     return (
         <div className={styles.container}>
             <div className={styles.headerContainer}>
-                <Image src={Logo} alt={""} className={styles.Logo}/>
+                <Image src={windowSize.width>309? Logo : LogoMinimalista} alt={""} className={windowSize.width > 309? styles.Logo : styles.LogoMinimalista}/>
                 <div className={styles.searchContainer}>
                     <form className={styles.formContainer}>
                         <input className={styles.inputContainer} placeholder="Buscar Tags..."></input>
