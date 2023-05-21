3a4ab45809084ae227642b56ff7f89c9e5d7b0e1
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-05-21 19:00:33 -0300

    fix: declaration of code

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,84 +1,97 @@
 "use client";
-import styles from './page.module.css';
-import { Mail, Lock, Hash } from 'lucide-react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Hash, Lock } from 'lucide-react';
 import api from '../../services/api';
+import styles from './page.module.css';
 
 interface ForgotPasswordProps {
-  onSubmit: (password: string, passwordConfirmation: string) => void;
+  onSubmit: (code: string, password: string, passwordConfirmation: string) => void;
 }
 
-export default function ForgotPassword(props: ForgotPasswordProps) {
-    const [password, setPassword] = useState<string>('');
-    const [passwordConfirmation, setPasswordConfirmation] = useState<string>('');
-  
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-  
-      try {
-        const response = await api.post('/change-password', { password, passwordConfirmation });
-        props.onSubmit(password, passwordConfirmation);
-      } catch (error) {
-        console.error('Erro ao alterar senha:', error);
-      }
-    };
-  
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setPassword(event.target.value);
-    };
-  
-    const handlePasswordConfirmationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setPasswordConfirmation(event.target.value);
-    };
-  
-    return (
-      <div className={styles.pageContainer}>
-        <div className={styles.changePasswordContainer}>
-          <h1 className={styles.changePasswordTitle}>Altere sua senha</h1>
-          <p className={styles.changePasswordText}>O código de alteração foi enviado para o seu e-mail</p>
-          <form className={styles.formContainer} onSubmit={handleSubmit}>
-            <div className={styles.fieldContainer}>
-              <Hash className={styles.iconContainer} size={24} />
-              <input
-                className={styles.inputContainer}
-                type="text"
-                id="code"
-                name="code"
-                placeholder="Código de alteração"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-              />
-            </div>
-            <div className={styles.fieldContainer}>
-              <Lock className={styles.iconContainer} size={24} />
-              <input
-                className={styles.inputContainer}
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Nova senha"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-              />
-            </div>
-            <div className={styles.fieldContainer}>
-              <Lock className={styles.iconContainer} size={24} />
-              <input
-                className={styles.inputContainer}
-                type="password"
-                id="passwordConfirmation"
-                name="passwordConfirmation"
-                placeholder="Confirme a nova senha"
-                value={passwordConfirmation}
-                onChange={handlePasswordConfirmationChange}
-                required
-              />
-            </div>
-            <input className={styles.changePasswordButton} type="submit" value="Alterar senha" />
-          </form>
-        </div>
+export default function ForgotPassword({ onSubmit }: ForgotPasswordProps) {
+  const [code, setCode] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (password !== passwordConfirmation) {
+      setError('A confirmação de senha não corresponde à nova senha.');
+      return;
+    }
+
+    try {
+      const response = await api.post('/change-password', { code, password, passwordConfirmation });
+      onSubmit(code, password, passwordConfirmation);
+    } catch (error) {
+      console.error('Erro ao alterar senha:', error);
+      setError('Erro ao alterar senha. Por favor, tente novamente.');
+    }
+  };
+
+  const handleCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCode(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordConfirmationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirmation(event.target.value);
+  };
+
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.changePasswordContainer}>
+        <h1 className={styles.changePasswordTitle}>Altere sua senha</h1>
+        <p className={styles.changePasswordText}>O código de alteração foi enviado para o seu e-mail</p>
+        <form className={styles.formContainer} onSubmit={handleSubmit}>
+          <div className={styles.fieldContainer}>
+            <Hash className={styles.iconContainer} size={24} />
+            <input
+              className={styles.inputContainer}
+              type="text"
+              id="code"
+              name="code"
+              placeholder="Código de alteração"
+              value={code}
+              onChange={handleCodeChange}
+              required
+            />
+          </div>
+          <div className={styles.fieldContainer}>
+            <Lock className={styles.iconContainer} size={24} />
+            <input
+              className={styles.inputContainer}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Nova senha"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <div className={styles.fieldContainer}>
+            <Lock className={styles.iconContainer} size={24} />
+            <input
+              className={styles.inputContainer}
+              type="password"
+              id="passwordConfirmation"
+              name="passwordConfirmation"
+              placeholder="Confirme a nova senha"
+              value={passwordConfirmation}
+              onChange={handlePasswordConfirmationChange}
+              required
+            />
+          </div>
+          {error && <p className={styles.error}>{error}</p>}
+          <input className={styles.changePasswordButton} type="submit" value="Alterar senha" />
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
