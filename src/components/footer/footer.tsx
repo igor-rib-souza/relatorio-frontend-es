1b286c3d5eac473e0196b7a7d4ae2c1081ca0498
Igor Souza <igorrs.rib@gmail.com>
2023-05-24 19:55:14 -0300

    feat: add buttong tag

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,22 +1,27 @@
+"use client"
 import styles from "./page.module.css";
-import { Calendar, Send} from "lucide-react";
+import { Calendar, Send } from "lucide-react";
+import Cookies from "js-cookie";
 
-export default function Footer(){
-    return(
+export default function Footer() {
+
+    const cookies = Cookies.get("user");
+
+    return (
         <div className={styles.container}>
             <div className={styles.searchContainer}>
-                    <form className={styles.formContainer}>
-                        <input className={styles.inputContainer} placeholder="Relatório"></input>
-                    </form>
+                <form className={styles.formContainer}>
+                    <input className={styles.inputContainer} placeholder="Relatório"></input>
+                </form>
+            </div>
+            <div className={styles.iconContainer}>
+                <div className={styles.iconBackground}>
+                    <Calendar className={styles.icon} />
                 </div>
-                <div className={styles.iconContainer}>
-                    <div className={styles.iconBackground}>
-                        <Calendar className={styles.icon}/>
-                    </div>
-                    <div className={styles.iconBackground}>
-                        <Send className={styles.icon}/>
-                    </div>
-                    </div>
+                <button className={styles.iconBackground} onClick={() => alert(cookies)}>
+                    <Send className={styles.icon} />
+                </button>
+            </div>
         </div>
     )
 }
