192d0c2aac686ec65daca72fa7174b0708ae81b2
Igor Souza <igorrs.rib@gmail.com>
2023-05-25 00:39:02 -0300

    chore: use prettier

--- src/components/relatorios/relatorios.tsx
+++ src/components/relatorios/relatorios.tsx
@@ -6,44 +6,44 @@ export default function Relatorios(relatorios: any) {
 
 
   return (
-<div className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.headerRelatorio}>
         <div className={styles.containerHeaderData}>
-          <p className={styles.date}>{data[0]["date"].slice(0,5)}</p>
+          <p className={styles.date}>{data[0]["date"].slice(0, 5)}</p>
           <ArrowRight className={styles.arrow} size={13} />
-          <p className={styles.date}>{data[data.length - 1]["date"].slice(0,5)}</p>
+          <p className={styles.date}>{data[data.length - 1]["date"].slice(0, 5)}</p>
         </div>
       </div>
       <div className={styles.containerRelatorio}>
         {data[0]._id != "" ?
-        data.map((relatorio: any, index: number) => (
-          <div key={index} className={styles.relatorio}>
-            <div className={styles.dateRelatorio}>
-              <p className={styles.textDate}>{relatorio.date.slice(8, 10)}/{relatorio.date.slice(5, 7)}/{relatorio.date.slice(0, 4)}</p>
-            </div>
-            <div className={styles.containerRelatorIndividual}>
-              <div className={styles.containerGlobalTimeTags}>
-                <div className={styles.containerTimeTags}>
-                  <Clock size={15} />
-                  <p className={styles.timeText}>{relatorio["startTime"]}</p>
-                  <ArrowRight
-                    className={styles.arrow}
-                    size={15}
-                    color={"black"}
-                  />
-                  <p className={styles.timeText}>{relatorio["endTime"]}</p>
+          data.map((relatorio: any, index: number) => (
+            <div key={index} className={styles.relatorio}>
+              <div className={styles.dateRelatorio}>
+                <p className={styles.textDate}>{relatorio.date.slice(8, 10)}/{relatorio.date.slice(5, 7)}/{relatorio.date.slice(0, 4)}</p>
+              </div>
+              <div className={styles.containerRelatorIndividual}>
+                <div className={styles.containerGlobalTimeTags}>
+                  <div className={styles.containerTimeTags}>
+                    <Clock size={15} />
+                    <p className={styles.timeText}>{relatorio["startTime"]}</p>
+                    <ArrowRight
+                      className={styles.arrow}
+                      size={15}
+                      color={"black"}
+                    />
+                    <p className={styles.timeText}>{relatorio["endTime"]}</p>
+                  </div>
+                  {data.map((tags: any, index: number) => (
+                    <p className={styles.text} key={index}>{relatorio["tags"][index]}</p>
+                  ))}
                 </div>
-                {data.map((tags: any, index: number) => (
-                  <p className={styles.text} key={index}>{relatorio["tags"][index]}</p>
-                ))}
-              </div>
-              <div className={styles.containerTextRelatorio}>
-                <p className={styles.text}>{relatorio["text"]}</p>
+                <div className={styles.containerTextRelatorio}>
+                  <p className={styles.text}>{relatorio["text"]}</p>
+                </div>
               </div>
             </div>
-          </div>
-        )):
-        <></>}
+          )) :
+          <></>}
       </div>
-    </div>  );
+    </div>);
 }
