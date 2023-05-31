38b264c11c2d413ee107a1421ac001353e724a2a
Igor Souza <igorrs.rib@gmail.com>
2023-05-31 00:38:40 -0300

    feat: create tags folder

---
+++ src/app/tags/page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/components/header/header';
+import styles from './page.module.css';
+import Menu from '@/components/menu/menu';
+
+const Tags = () => {
+
+    return (
+        <div className={styles.container}>
+            <Header />
+            <Menu />
+
+        </div>
+    )
+}
+
+export default Tags;
