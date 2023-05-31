b5507f0591efefcf25c24753cc818ff226c902fe
Igor Souza <igorrs.rib@gmail.com>
2023-05-30 22:28:38 -0300

    feat: add callback and change api call

--- src/app/relatorios/page.tsx
+++ src/app/relatorios/page.tsx
@@ -1,148 +1,157 @@
 "use client"
+import React, { useState, useEffect, useCallback } from "react";
 import Header from "../../components/header/header";
 import Menu from "../../components/menu/menu";
 import styles from "./page.module.css";
 import Relatorio from "../../components/relatorios/relatorios";
-import api from "../../services/api"
-import { SetStateAction, useEffect, useState } from "react";
+import api from "../../services/api";
 import Cookies from "js-cookie";
 import { Calendar, Send } from "lucide-react";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
-import { addHours, format } from 'date-fns';
+import { addHours, format } from "date-fns";
 
-
-
-export default function Relatorios() {
-
+const Relatorios = () => {
   const [text, setText] = useState("");
   const [startDate, setStartDate] = useState(new Date());
-  const [startTime, setStartTime] = useState("00:00")
   const [showTime, setShowTime] = useState(false);
-
-
-  const handleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
-    setText(event.target.value);
-  };
-
   const [reports, setReports] = useState({
-    "reports": [
+    reports: [
       {
-        "_id": "",
-        "user": "",
-        "date": "0000-00-00T00:00:00.000Z",
-        "startTime": "",
-        "endTime": "",
-        "text": "",
-        "tags": [
-          ""
-        ],
-        "createdAt": "",
-        "updatedAt": "",
-        "__v": 0
-
-      }
-    ]
+        _id: "",
+        user: "",
+        date: "0000-00-00T00:00:00.000Z",
+        startTime: "",
+        endTime: "",
+        text: "",
+        tags: [""],
+        createdAt: "",
+        updatedAt: "",
+        __v: 0,
+      },
+    ],
   });
+
   const cookies:any = Cookies.get("user");
   const user = JSON.parse(cookies);
-  const adm = user.user.type == "adm";
+  const adm = user.user.type === "adm";
 
-  async function getReports() {
-    await api.get(`report/${user.user._id}/01010000/12129999`, {
-      headers: {
-        'Authorization': `Bearer ${user.token}`,
-      }
-    }).
-      then((response) => { if (response.data.reports[0]._id != "") { setReports(response.data); } })
-      .catch((error) => { console.log(error.data) })
-  }
+  const handleChange = useCallback((event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setText(event.target.value);
+  }, []);
 
-  async function getReportsAdm() {
-    await api.get(`report/all/${user.user._id}/01010000/12129999`, {
-      headers: {
-        'Authorization': `Bearer ${user.token}`,
+  const getReports = useCallback(async () => {
+    try {
+      const response = await api.get(`report/${user.user._id}/01010000/12129999`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      if (response.data.reports[0]._id !== "") {
+        setReports(response.data);
       }
-    }).
-      then((response) => { if (response.data.reports[0]._id != "") { setReports(response.data); } })
-      .catch((error) => { console.log(error.data) })
-  }
+    } catch (error:any) {
+      console.log(error.data);
+    }
+  }, [user.user._id, user.token]);
+
+  const getReportsAdm = useCallback(async () => {
+    try {
+      const response = await api.get(`report/all/${user.user._id}/01010000/12129999`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      if (response.data.reports[0]._id !== "") {
+        setReports(response.data);
+      }
+    } catch (error:any) {
+      console.log(error.data);
+    }
+  }, [user.user._id, user.token]);
+
+  const sendReport = useCallback(async () => {
+    try {
+      await api.post(
+        `/report/${user.user._id}`,
+        {
+          user: user.user._id,
+          date: format(startDate, "dd/MM/yyyy"),
+          startTime: format(startDate, "HH:mm"),
+          endTime: format(addHours(startDate, 1), "HH:mm"),
+          text: text,
+          tags: ["tag"],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      setText("");
+    } catch (error:any) {
+      console.log(error.response.data);
+    }
+  }, [startDate, text, user.user._id, user.token]);
 
   useEffect(() => {
-    {
-      adm ?
-        getReports()
-        :
-        getReportsAdm()
+    if (adm) {
+      getReports();
+    } else {
+      getReportsAdm();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [reports])
-
-  async function sendReport() {
-
-    await api.post(`/report/${user.user._id}`, {
-      user: user.user._id,
-      date: format(startDate, "dd/MM/yyyy"),
-      startTime: format(startDate, "HH:mm"),
-      endTime: format(addHours(startDate, 1), "HH:mm"),
-      text: text,
-      tags: ["tag"]
-    },
-      {
-        headers: {
-          'Authorization': `Bearer ${user.token}`,
-        }
-      }
-    ).then((response) => setText("")
-    ).catch((error) => console.log(error.response.data))
-  }
+  }, [adm, getReports, getReportsAdm]);
 
   return (
     <div className={styles.container}>
       <Header />
       <div className={styles.container2}>
         <div className={`${styles.datePicker} ${styles.centered}`}>
-          {showTime ?
-            <DatePicker
-              selected={startDate}
-              onChange={(date:any) => setStartDate(date)}
-              showTimeSelect
-              inline
-            />
-            :
-            <></>
-          }
+          {showTime && (
+            <DatePicker selected={startDate} onChange={(date:any) => setStartDate(date)} showTimeSelect inline />
+          )}
         </div>
-        <div>
-          <Menu />
-        </div>
+        <Menu />
         <div className={styles.container3}>
-          {<Relatorio relatorios={reports}/>}
+          <Relatorio relatorios={reports} />
 
-          {/*
-            A parte abaixo é referente ao footer 
-          */}
+          {/* The section below refers to the footer */}
           <div className={styles.container4}>
             <div className={styles.searchContainer}>
               <form className={styles.formContainer}>
-                <input className={styles.inputContainer} placeholder="Relatório" value={text} onChange={handleChange}></input>
+                <input
+                  className={styles.inputContainer}
+                  placeholder="Relatório"
+                  value={text}
+                  onChange={handleChange}
+                />
               </form>
             </div>
             <div className={styles.iconContainer}>
-
               <div className={styles.iconBackground} onClick={() => setShowTime(!showTime)}>
                 <Calendar className={styles.icon} />
               </div>
-              <button className={styles.iconBackground} onClick={() => { sendReport(); { adm ? getReportsAdm() : getReports() } setShowTime(false) }}>
+              <button
+                className={styles.iconBackground}
+                onClick={() => {
+                  sendReport();
+                  if (adm) {
+                    getReportsAdm();
+                  } else {
+                    getReports();
+                  }
+                  setShowTime(false);
+                }}
+              >
                 <Send className={styles.icon} />
               </button>
             </div>
           </div>
-          {/*
-            A div acima é o final do footer 
-          */}
+          {/* The section above is the end of the footer */}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Relatorios;
