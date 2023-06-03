7993d1cad57bc2f63107f3b76474232d387c3bde
Igor Souza <igorrs.rib@gmail.com>
2023-06-03 18:03:16 -0300

    feat: bugfix tag

--- src/components/Tag/tagField.tsx
+++ src/components/Tag/tagField.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css"
 
 const Tag: React.FC<{ textContent:string, color:string, isChecked: boolean; onChange: (event: ChangeEvent<HTMLInputElement>) => void }> = ({ textContent, color, isChecked, onChange }) => {
     return (
-      <label style={styles.checkbox}>
+      <label className={styles.checkbox}>
         <input type="checkbox" checked={isChecked} onChange={onChange} />
         {textContent}
       </label>
